60b3428f172d73450cece3506cba9222c7b0728d
andre-menezes <andre.menezes@vnda.com.br>
2021-02-08 21:53:42 -0300

    Ajuste no title

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -5,7 +5,7 @@ function IndexHead() {
   return (
     <Head>
       <meta property="og:image" content={db.bg} />
-      <title>CSS Quiz</title>
+      <title>PoliQuiz</title>
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
     </Head>
